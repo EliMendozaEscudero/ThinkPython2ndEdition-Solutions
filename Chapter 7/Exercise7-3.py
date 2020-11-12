3acc004b82a4befe3ad6e54819428b1603687281
Eli <elimendoza041@gmail.com>
2020-11-11 23:32:01 -0700

    Exersice7-3 solved.

---
+++ Chapter 7/Exercise7-3.py
@@ -0,0 +1,17 @@
+import math 
+
+def estimate_pi():
+    sum = 0
+    subsum = 1
+    k =0
+    while(subsum > 1e-15):
+        subsum = (math.factorial(4*k)*(1103 + 26390*k))/(math.factorial(k)**4 * 396**(4*k)) 
+        sum += subsum
+        k += 1
+
+    pi = 1 / ((2 * math.sqrt(2)/9801) *sum) 
+    return pi
+
+if __name__ == '__main__':
+    print(estimate_pi())
+        
